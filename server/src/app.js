a7c215843982025e61017c635ebd855452e115d4
Claude Vaillancourt <vaillancourt.c@gmail.com>
2021-10-14 12:47:01 -0400

    Added sequelize and sqlite3 with User model

--- server/src/app.js
+++ server/src/app.js
@@ -2,21 +2,30 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models') // resolve to ./models/index.js
+const config = require('./config/config')
 
+var path = require('./models');
+console.log(". = %s", path);
+//console.log("__dirname = %s", path.resolve(__dirname));
+
+// Create the express app object
 const app = express()
 
-//app.use(morgan('combined'))
+// Add app middleware
+//app.use(morgan('combined'))  // This provides the client user-agent info
 app.use(morgan('common'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-    res.send({
-        message: 'Hello World!'
-    })
-})
+// Define routes
+require('./routes')(app)
 
-const port = process.env.PORT || 8080
-app.listen(port, () => {
-    console.log('Express server started on port ' + port)
-})
+// Create the database and start the server
+sequelize.sync()
+    .then(() => {
+        app.listen(config.port, () => {
+        console.log(`Express server started on ${config.port}`)
+        })
+    })
+    .catch(() => console.log('Failed to connect database'))
---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    port: process.env.PORT || 8080,
+    db: {
+        database: process.env.DB_NAME || 'clevrbooks',
+        user: process.env.DB_USER || 'clevrcode',
+        password: process.env.DB_PASSWORD || '12345678',
+        options: {
+            dialect: process.env.DIALECT || 'sqlite',
+            host: process.env.HOST || 'localhost',
+            storage: './clevrbooks.sqlite'
+        }
+    }
+}
