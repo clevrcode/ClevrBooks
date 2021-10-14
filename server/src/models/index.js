42f6bc0248726bca6567377ea4717de61181249f
Claude Vaillancourt <vaillancourt.c@gmail.com>
2021-10-14 17:06:30 -0400

    Minor cleanup

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -12,7 +12,7 @@ const connection = new Sequelize(
 )
 
 fs.readdirSync(__dirname).filter((file) => file !== 'index.js').forEach((file) => {
-    //const model = sequelize.import(path.join(__dirname, file))
+    console.log(`Import model from ${file}`)
     const model = require(path.join(__dirname, file))(connection, Sequelize)
     db[model.name] = model
 })
