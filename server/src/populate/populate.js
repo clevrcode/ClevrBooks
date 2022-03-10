6275ad0e4b3d2521b1752634d8ae77e2d23efaf0
Claude Vaillancourt <vaillancourt.c@gmail.com>
2022-03-10 11:41:16 -0500

    Modified to handle split transactions properly

--- server/src/populate/populate.js
+++ server/src/populate/populate.js
@@ -11,24 +11,19 @@ const fs = require('fs')
 const path = require('path')
 const sequelize = require('sequelize')
 const { User, Account, Entry, Category, Subcategory } = require('../models');
+const account = require('../models/account');
 const subcategory = require('../models/subcategory');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 
+// console.log(convertAmount('"-1,234.56"'))
+// console.log(convertAmount('123.45'))
 function convertAmount(amount) {
     if (amount.startsWith('"')) {
         amount = amount.replace(/,/g, '')
         amount = /"(?<amnt>.*)"/.exec(str_amount).groups.amnt
     }
     return parseFloat(amount)
-}
-
-// console.log(convertAmount('"-1,234.56"'))
-// console.log(convertAmount('123.45'))
-const fetchUser = async (username) => {
-    return User.findOne({
-        where: { email: username }
-    })
 }
 
 const AccountAliases = {
@@ -43,26 +38,29 @@ const AccountAliases = {
     "Streetwise Balanced Income Fund": "Tangerine Streetwise"
 }
 
-const csvFiles = [
-    "ArgentCash.csv", 
-    "BanqueNationaleCELI.csv", 
-    "BanqueNationale.csv", 
-    "BanqueNationaleMargeDeCredit.csv",
-    "BanqueNationaleSaving.csv", 
-    "CV_Consultant.csv", 
-    "Desjardins.csv", 
-    "DesjardinsMargeDeCredit.csv",
-    "DesjardinsTaxe.csv", 
-    // "Home_811TK.csv", 
-    // "HondaCivic.csv", 
-    "MasterCard.csv", 
-    // "Retraite_L3.csv", 
-    "TangerineCELI.csv",
-    "Tangerine.csv", 
-    "TangerineKickstart.csv", 
-    "TPS_TVQ.csv", 
-    "VISA.csv"
-]
+const csvFiles = {
+    user: 'vaillancourt.c@gmail.com',
+    files: [
+        // "ArgentCash.csv", 
+        // "BanqueNationaleCELI.csv", 
+        "BanqueNationale.csv", 
+        // "BanqueNationaleMargeDeCredit.csv",
+        // "BanqueNationaleSaving.csv", 
+        // "CV_Consultant.csv", 
+        // "Desjardins.csv", 
+        // "DesjardinsMargeDeCredit.csv",
+        // "DesjardinsTaxe.csv", 
+        // // "Home_811TK.csv", 
+        // // "HondaCivic.csv", 
+        // "MasterCard.csv", 
+        // // "Retraite_L3.csv", 
+        // "TangerineCELI.csv",
+        // "Tangerine.csv", 
+        // "TangerineKickstart.csv", 
+        // "TPS_TVQ.csv", 
+        // "VISA.csv"
+    ]
+}
 
 async function connectAndAuthenticate () {
     try {
@@ -74,105 +72,91 @@ async function connectAndAuthenticate () {
         console.error('Unable to connect to the database:', error);
         return false
     }
-    console.log('exiting')
 }
 
 const insertEntry = async (data) => {
-    const amount = data.amount
-    Entry.create(data)
-    .then(
-        entry => {
-            console.log(`Created new entry for '${entry.payee}'`)
-            return Account.increment('currentBalance', { by: amount, where: { id: data.accountId }})
-        }
-    )
-    .then(
-        ret => {
-            console.log(`Account ${data.payee} incremented successfully`)
-            console.log(ret)
-            // if (data.xferToAccount) {
-            //     // Create the corresponding entry in the account transfered to...
-            //     let xdata = data
-            //     xdata.accountId = data.category
-            //     xdata.amount = -data.amount
-            //     xdata.checkNumber = null
-            //     xdata.type = null
-            //     xdata.category = data.accountId
-            //     console.log('Create cross-entry for ' + xdata)
-            //     return Entry.create(xdata)
-            // }
-        }
-    )
-    // .then(
-    //     entry => {
-    //         if (entry) {
-    //             console.log(`Cross-Entry created for '${data.payee}'`)
-    //             console.log(entry)
-    //             return Account.increment('currentBalance', { by: entry.amount, where: { id: entry.accountId }})
-    //         }
-    //     }
-    // )
-    // .then(
-    //     account => {
-    //         if (account) {
-    //             console.log(`Account incremented successfully`)
-    //             console.log(`Entry entered successfully!!!`)
-    //         }
-    //     }
-    // )
-    .catch(
-        error => {
-            console.error(error)
-        }        
-    )
+    try {
+        const entry = await Entry.create(data)
+        console.log(`Created new entry for '${data.account}'`)
+        await Account.increment('currentBalance', { by: data.amount, where: { id: data.accountId }})
+        console.log(`Account ${data.account} incremented successfully`)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const insertSubCategory = async (id, subcat) => {
+    try {
+        const sub = await Subcategory.create({ name: subcat, category: id })
+        console.log(`Subcategory '${sub.name}' created with id: ${sub.id}' `)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 const insertCategory = async (data) => {
-    Category.create({ name: data.name, type: data.type })
-    .then(
-        category => {
-            console.log(`Created new category for '${category.name}'`)
-            data.subcategories.forEach((subcat) => {
-                Subcategory.create({ name: subcat, category: category.id })
-                .then((sub) => {
-                    console.log(`Subcategory '${sub.name}' created with id: ${sub.id}' `)
-                })
-            })
-        }
-    )
-    .catch( (error) => {
+    try {
+        const category = await Category.create({ name: data.name, type: data.type })
+        console.log(`Created new category for '${category.name}'`)
+        data.subcategories.forEach((subcat) => {
+            insertSubCategory(category.id, subcat)
+        })
+    } catch (error) {
         console.log(error)
-    })
+    }
+}
+
+const renameAccount = (name) => {
+    let accName = name
+    if (name.startsWith('[')) {
+        accName = name.substring(1, name.length - 1)
+    }
+    if (accName in AccountAliases) {
+       accName = AccountAliases[accName]
+    }
+    if (name.startsWith('[')) {
+        accName = '[' + accName + ']'
+    }
+    return accName
 }
 
 const insertAccount = async (acct) => {
     try {
-        const user = await fetchUser(acct.user)
+        const user = await User.findOne({ where: { email: acct.user } })
         if (user) {
             console.log('Create acount for user ' + user)
-            Account.create({ name: acct.name, 
+            const account = await Account.create({ name: acct.name, 
                 description: acct.description,
                 initBalance: acct.initBalance,
                 currentbalance: acct.currentbalance,
-                currency: acct.currentbalance,
+                currency: acct.currency,
                 userId: user.id })
-            .then(
-                account => {
-                    console.log(`Account for '${account.name}' created successfully'`)
-                }
-            )
-            .catch (error => {
-                console.error(error)
-            }) 
-        
+            console.log(`Account for '${account.name}' created successfully'`)      
         } else {
-            console.log(`Couldn't found ${acct.user}`)
+            console.log(`Couldn't found user: ${acct.user}`)
         }
     } catch (error) {
         console.log(error)
     }
 }
 
+function renameType(type) {
+    if (type) {
+        let newType = type
+        if (newType === 'GAB') {
+            newType = 'ATM'
+        } else {
+            if (newType.indexOf(' ') >= 3) {
+                newType = newType.substring(0, newType.indexOf(' '))
+            } 
+            if (newType.endsWith('.')) {
+                newType = newType.substring(0, newType.length - 1)
+            }
+        }
+        return newType
+    }
+    return type
+}
 
 //==========================================================
 // Show environment
@@ -203,54 +187,122 @@ accts.forEach((acct) => {
 //==========================================================
 // Populate Entries tables
 
+csvFiles.files.forEach((file) => {
+    parseCsvFile('../doc/data/' + file, csvFiles.user)
+})
+
 //parseCsvFile(seedfile, UserId)
 //process.exit() <=== ???? Do not call exit, it prevents all async methods from execute.
 
-function parseCsvFile(file, userId) {
-    console.log(`Open ${file} for user id : ${userId}...`)   
-    fs.readFile(file, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err)
+async function processEntry(entry, userId) {
+    try {
+        const account = await Account.findOne({ where: { name: entry.account, userId: userId } })
+        let category = null
+        let subcategory = null
+        if (entry.xferToAccount) {
+            const name = entry.category.substring(1, entry.category.length - 1)
+            const acc = await Account.findOne({ where: { name: name, userId: userId } })
+            category = acc.id
         } else {
-            const re = new RegExp(',(?<date>\\d\\d\\d\\d-[01]\\d-[0123]\\d),(?<account>[^,]+),(?<check>[^,]*),(?<payee>[^,]+),(?<memo>[^,]*),(?<category>[^,]+),,(?<cleared>[R]*),(?<amount>.+)')
-            const lines = data.split('\n')
-            lines.forEach( line => {
-                console.log(line)
-                const items = re.exec(line)
-                if (items) {
-                    console.log(`find or create account ${items.groups.account}`)
-                    Account.findOrCreate({
-                        where: { name: items.groups.account },
-                        defaults: {
-                            description: items.groups.account,
-                            userId: userId
-                        }
-                    })
-                    .then(
-                        account => {
-                            console.log(`insert entry in account '${account[0].name}'`)
-                            console.log(`account '${account[0].name}' was${ account[1] ? ' ' : ' not '}created`)
-                            const checkNumber = isNaN(parseInt(items.groups.check)) ? null : parseInt(items.groups.check)
-                            const type = checkNumber === null ? items.groups.check : null
-                            insertEntry({
-                                date: items.groups.date,
-                                checkNumber: checkNumber,
-                                type: type,
-                                payee: items.groups.payee,
-                                memo: items.groups.memo,
-                                category: items.groups.category,
-                                xferToAccount: items.groups.category.startsWith('['),
-                                cleared: items.groups.cleared === 'R',
-                                amount: convertAmount(items.groups.amount),
-                                accountId: account[0].id
-                            })
-                        }
-                    ).catch(
-                        (err) => console.log('fetchAccount error! ' + err)
-                    )
-                }
-            })
+            const names = entry.category.split(':')
+            const cat = await Category.findOne({ where: { name: names[0] } })
+            category = cat.id
+            if (names.length > 1) {
+                const subcat = await Subcategory.findOne({ where: { name: names[1], category: cat.id }})
+                subcategory = subcat ? subcat.id : null
+            }
         }
-    })
+
+        if (account && category) {
+            console.log(`insert entry in account '${account.name}'`)
+            entry.accountId = account.id
+            entry.category = category
+            entry.subcategory = subcategory
+            insertEntry(entry)
+        } else {
+            console.log(`Account '${entry.account}' not found!`)
+        }
+    } catch (err) {
+        (err) => console.log(err)
+    }
+}
+
+async function parseCsvFile(file, userEmail) {
+    try {
+        const user = await User.findOne({ where: { email: userEmail } })
+        if (!user) {
+            console.log('User not found: ' + userEmail)
+            return
+        }
+        console.log(`Open ${file} for user id : ${user.id} ...`)
+        fs.readFile(file, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err)
+            } else {
+                let linecount = 0
+                let balance = 0.0
+                let entry = null
+                let defer = false
+                const re = new RegExp(',(?<date>\\d\\d\\d\\d-[01]\\d-[0123]\\d),(?<account>[^,]+),(?<check>[^,]*),(?<payee>[^,]+),(?<memo>[^,]*),(?<category>[^,]+),(?<tag>[^,]*),(?<cleared>[R]*),(?<amount>[0-9\-.]+),')
+                const re2 = new RegExp(',,,,,(?<memo>[^,]*),(?<category>[^,]*),(?<tag>[^,]*),(?<cleared>[R]*),(?<amount>[0-9\-.]+),')
+                const lines = data.split('\n')
+                lines.forEach( line => {
+                    const items = re.exec(line)
+                    if (items) {
+                        linecount++
+                        // console.log(line)
+                        //console.log(convertAmount(items.groups.amount).toFixed(2))
+                        if (defer && entry) {
+                            processEntry(entry, user.id)
+                            entry = null
+                        }
+                        balance += convertAmount(items.groups.amount)
+                        const checkNumber = isNaN(parseInt(items.groups.check)) ? null : parseInt(items.groups.check)
+                        defer = !checkNumber && (items.groups.check && (items.groups.check.length > 10) && (items.groups.check.endsWith('S')))
+                        const type = renameType((checkNumber === null) ? items.groups.check : null)
+                        const accountName = renameAccount(items.groups.account)
+                        const categoryName = renameAccount(items.groups.category)
+                        const xferToAccount = categoryName.startsWith('[')
+                        // if (checkNumber) {
+                        //     console.log(`Check #${checkNumber}`)
+                        // } else if (type) {
+                        //     console.log(`type: ${type}`)
+                        // } else {
+                        //     console.log('Missing type or check# :' + line)
+                        // }
+                        entry = {
+                            account: accountName,
+                            date: items.groups.date,
+                            checkNumber: checkNumber,
+                            type: type,
+                            payee: items.groups.payee,
+                            memo: items.groups.memo,
+                            category: categoryName,
+                            xferToAccount: xferToAccount,
+                            cleared: items.groups.cleared === 'R',
+                            amount: convertAmount(items.groups.amount),       
+                        }
+                        if (!defer) {
+                            processEntry(entry, user.id)
+                        }
+                    } else if (defer && entry) {
+                        const subitms = re2.exec(line)
+                        if (subitms) {
+                            linecount++
+                            //console.log(convertAmount(subitms.groups.amount).toFixed(2))
+                            const amnt = convertAmount(subitms.groups.amount)
+                            balance += amnt
+                            entry.amount += amnt
+                            // console.log(line)
+                        }
+                    }
+                })
+                console.log(`Processed ${linecount} lines`)
+                console.log(`Ending Balance: ${balance.toFixed(2)}`)
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
 
